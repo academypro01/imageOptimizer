18431669e51e0d96a46c2d91df34c110a829347e
academy01 <80899303+academypro01@users.noreply.github.com>
2022-03-15 01:16:42 +0330

    Add FA locale

---
+++ lang/fa/homePage.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'title' => 'Image Optimizer!',
+    'fileInputLabel' => 'Choose an Image',
+    'configBoxTitle' => 'Config Box',
+    'width' => 'Width(px):',
+    'height' => 'Height(px):',
+    'widthPlaceholder' => 'Enter Image width px',
+    'heightPlaceholder' => 'Enter Image height px',
+    'compress' => 'Compress ',
+    'startButton' => 'Start',
+    'alert' => 'Image Optimized, now you can download it.',
+    'downloadButton' => 'Download'
+];
